376a7279fc62589ec524c14f65467e503844ec52
Zdenek ULRYCH <ulrychz@office365.zcu.cz>
2024-02-19 16:23:38 +0100

    Přidejte soubory projektu.

---
+++ Po1450Obdelnik/Model/Obdelnik.cs
@@ -0,0 +1,30 @@
+﻿namespace Po1450Obdelnik.Model
+{
+    public class Obdelnik
+    {
+        public int StranaA { get; set; }
+        public int StranaB { get; set; }
+        public int Obvod { get; set; }
+        public int Obsah { get; set; }
+        public double Uhlopricka { get; set; }
+        private void SpoctiObvod()
+        {
+            Obvod = 2*(StranaA + StranaB);
+        }
+
+        private void SpoctiObsah()
+        {
+            Obsah = (StranaA * StranaB);
+        }
+        private void SpoctiUhlopricku ()
+        {
+            Uhlopricka = Math.Sqrt(StranaA * StranaA + Math.Pow(StranaB,2));
+        }
+        public void Vypocet()
+        {
+            SpoctiObvod();
+            SpoctiObsah();
+            SpoctiUhlopricku();
+        }
+    }
+}
